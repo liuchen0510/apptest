237e1b3ad3ed5b9f0d15bf0c12140387ff2cabaf
liuhaixin <1689449711@qq.com>
2019-05-29 11:56:10 +0800

    renew

---
+++ liuhaixin/YOUTEST/src/main/java/com/testcases/login.java
@@ -0,0 +1,22 @@
+package com.testcases;
+
+import com.base.BasePrepare;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.BeforeMethod;
+
+public class login extends BasePrepare {
+    @BeforeMethod
+    public void login() throws InterruptedException {
+        Thread.sleep(5000);
+
+        driver.findElementByName("我").click();
+
+        driver.findElementById(" neet.com.youjidemo:id/iv_head_sculpture").click();
+        Thread.sleep(5000);
+        driver.findElementById("neet.com.youjidemo:id/et_userPhone").sendKeys("120331");
+        driver.findElement(By.className("android.widget.RelativeLayout")).sendKeys("120331");
+        driver.findElementById("neet.com.youjidemo:id/btn_login").click();
+}
+}
