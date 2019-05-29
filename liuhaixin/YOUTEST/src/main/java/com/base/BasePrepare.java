6ed90ffa159c190d2eff94882b05f58b419bbd4f
liuhaixin <1689449711@qq.com>
2019-05-29 17:14:10 +0800

    new

--- liuhaixin/YOUTEST/src/main/java/com/base/BasePrepare.java
+++ liuhaixin/YOUTEST/src/main/java/com/base/BasePrepare.java
@@ -3,6 +3,7 @@ package com.base;
 import com.pages.HomePage;
 import com.utils.Action;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 
 import java.net.URL;
@@ -19,18 +20,23 @@ public class BasePrepare {
     @BeforeSuite
     public   void AndroidSettings() throws Exception {
         DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("deviceName", "Nexus One API 28 ");//设置需要调试模拟器的名字
+        capabilities.setCapability("deviceName", "emulator-5554");//设置需要调试模拟器的名字
         capabilities.setCapability("platformVersion", "8.0.0");//设置模拟器的版本
 
         capabilities.setCapability("appPackage", "neet.com.youjidemo");
         capabilities.setCapability("appActivity", ".MainActivity");
         capabilities.setCapability("unicodeKeyboard", "True"); //支持中文输入
-        capabilities.setCapability("resetKeyboard", "True");   //支持中文输入，必须两条都配置
+        capabilities.setCapability("resetKeyboard", "True");
+        capabilities.setCapability("noReset", false  );//支持中文输入，必须两条都配置
         driver = new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 
        // action = new Action(driver);
         action=new Action(driver);
         homePage =new HomePage(driver);
+    }
+    @AfterSuite
+    public void tearDown(){
+        driver.quit();
     }
 }
