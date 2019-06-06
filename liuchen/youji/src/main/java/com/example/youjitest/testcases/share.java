4bf4113ed9a4f0f5935b2e8d95cfdeca355e0480
刘宸 <825147384@qq.com>
2019-06-06 09:38:58 +0800

    Create share.java

---
+++ liuchen/youji/src/main/java/com/example/youjitest/testcases/share.java
@@ -0,0 +1,25 @@
+package com.testcases;
+
+import com.base.BaseTest;
+import com.pages.ShareActivity;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class share extends BaseTest {
+    @Test(dataProvider = "")
+    public void notes(){
+        ShareActivity add=new  ShareActivity(getDriver());
+        add.notes("my first share!");
+        System.out.print(getDriver().currentActivity());
+        Assert.assertEquals(".MainActivity",getDriver().currentActivity());
+    /*@Test
+    public void addnotes() throws InterruptedException {
+        Thread.sleep(3000);
+        driver.findElementByName("收藏").click();
+        driver.findElementByName("编辑").click();
+        driver.findElementById("neet.com.youjidemo:id/list").sendKeys("my first share!");
+}*/
+}
+}
