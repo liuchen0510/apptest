fb7a44a91ec4f0744014b0b33721667c861f682f
刘宸 <825147384@qq.com>
2019-06-13 10:24:59 +0800

    Update and rename CheckMessageAtPage.java to CheckPage1.java

--- liuchen/youji/src/main/java/com/example/youjitest/pages/CheckPage1.java
+++ liuchen/youji/src/main/java/com/example/youjitest/pages/CheckPage1.java
@@ -6,29 +6,29 @@ import org.openqa.selenium.support.FindBy;
 
 import io.appium.java_client.AppiumDriver;
 
-public class CheckMessageAtPage {
+public class CheckPage1 {
     private AppiumDriver driver;
     @FindBy(xpath = "\"//*[@text='消息']\"")
-    private WebElement txt_title;
+    private WebElement title1;
     @FindBy(xpath = "\"//*[@text='消息']\"")
-    private WebElement txt_title1;
+    private WebElement title2;
     @FindBy(xpath = "\"//*[@text='@的消息'\"")
-    private WebElement txt_title2;
+    private WebElement title3;
     @FindBy(id = "neet.com.youjidemo:id/ib_collection")
-    private WebElement txt_col;
+    private WebElement collection;
     Actions action;
 
-    public CheckMessageAtPage(AppiumDriver driver) {
+    public CheckPage1(AppiumDriver driver) {
 
         this.driver = driver;
         action = new Actions(driver);
     }
 
     public void cmap() {
-        action.click(txt_title);
         action.click(txt_title1);
         action.click(txt_title2);
-        action.click(txt_col);
+        action.click(txt_title3);
+        action.click(collection);
 
     }
 }
