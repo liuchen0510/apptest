d1db6edcaf7d49467330926c6eeca7ed2a641557
刘宸 <825147384@qq.com>
2019-06-13 10:03:35 +0800

    Update and rename AddcommentPage.java to AddPage2.java

--- liuchen/youji/src/main/java/com/example/youjitest/pages/AddPage2.java
+++ liuchen/youji/src/main/java/com/example/youjitest/pages/AddPage2.java
@@ -6,20 +6,20 @@ import org.openqa.selenium.support.FindBy;
 
 import io.appium.java_client.AppiumDriver;
 
-public class AddcommentPage {
+public class AddPage2{
     private AppiumDriver driver;
     @FindBy(id="neet.com.youjidemo:id/tab_icon")
-    private WebElement txt_icon;
+    private WebElementicon;
     @FindBy(id="neet.com.youjidemo:id/et_share_passage")
-    private WebElement txt_passage;
+    private WebElement passage;
     @FindBy(xpath = "\"//*[@text='点击增加图片或视频']\"")
-    private WebElement txt_title;
+    private WebElement title;
     @FindBy(className="android.widget.RelativeLayout")
-    private WebElement txt_layout;
+    private WebElement layout;
     @FindBy(id="com.google.android.apps.photos:id/image")
-    private WebElement txt_image;
+    private WebElement image;
     @FindBy(xpath = "\"//*[@text='上传']\"")
-    private WebElement txt_shangchuan;
+    private WebElement upload;
     Actions action;
     public AddcommentPage(AppiumDriver driver) {
 
@@ -27,13 +27,13 @@ public class AddcommentPage {
         action = new Actions(driver);
     }
 
-    public void addcomment(String text) {
-        action.click( txt_icon);
-        action.sendKeys(txt_passage,text);
-        action.click(txt_title);
-        action.click(txt_layout);
-        action.click(txt_image);
-        action.click(txt_shangchuan);
+    public void add(String text) {
+        action.click(icon);
+        action.sendKeys(passage,text);
+        action.click(title);
+        action.click(layout);
+        action.click(image);
+        action.click(upload);
 
     }
 
