e64adfb3ec6029016db380ea3dffed7e10f1604a
刘宸 <825147384@qq.com>
2019-06-21 15:27:23 +0800

    Update and rename Shopping.java to Shop.java

---
+++ liuchen/youji/src/main/java/com/example/youjitest/testcases/Shop.java
@@ -0,0 +1,24 @@
+package com.testcases;
+
+import com.base.BaseTest;
+import com.pages.ShoppingMessagePage;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Shop extends BaseTest {
+    @Test(dataProvider = "")
+    public void shop(){
+        ShopPage shop =new ShopPage(getDriver());
+        System.out.print(getDriver().currentActivity());
+        Assert.assertEquals(".MainActivity",getDriver().currentActivity());
+    }
+   /* @Test
+    public void shop() throws InterruptedException {
+        Thread.sleep(3000);
+        driver.findElementByName("逛街").click();
+        driver.findElementById("neet.com.youjidemo:id/des").click();
+        Thread.sleep(3000);
+        driver.findElementById("neet.com.youjidemo:id/good").click();
+    }*/
+}
