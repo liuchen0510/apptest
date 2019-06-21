b808848b4e1969b0628e7f054f6382b8cdecbb39
刘宸 <825147384@qq.com>
2019-06-21 15:09:30 +0800

    Update Check.java

--- liuchen/youji/src/main/java/com/example/youjitest/testcases/Check.java
+++ liuchen/youji/src/main/java/com/example/youjitest/testcases/Check.java
@@ -9,22 +9,22 @@ import org.testng.annotations.Test;
 
 public class Check extends BaseTest {
     @Test(dataProvider = "")
-    public void ctascps(){
-        ChecktextandsendcommentPage checktextandsendcommentPage =new ChecktextandsendcommentPage(getDriver());
-        checktextandsendcommentPage.ctascp("bug还没找到！");
+    public void check(){
+        ChecktPage1 check1 =new CheckPage1(getDriver());
+        check1.check1("bug未找到！");
         System.out.print(getDriver().currentActivity());
         Assert.assertEquals(".MainActivity",getDriver().currentActivity());
     }
 
     /*@Test
-    public void checktextandsendcomment() throws InterruptedException {
-        Thread.sleep(5000);
+    public void check() throws InterruptedException {
+        Thread.sleep(3000);
         driver.findElementByName("首页").click();
-        driver.findElementById("neet.com.youjidemo:id/ib_good").click();
-        driver.findElementById("neet.com.youjidemo:id/ib_judges").click();
-        driver.findElementById("neet.com.youjidemo:id/tv_collectNum").click();
+        driver.findElementById("neet.com.youjidemo:id/good").click();
+        driver.findElementById("neet.com.youjidemo:id/judges").click();
+        driver.findElementById("neet.com.youjidemo:id/collection").click();
         driver.findElementByName("点击发表你的评论").click();
-        driver.findElementById("neet.com.youjidemo:id/comment_content").sendKeys(" bug");
-        driver.findElementById("neet.com.youjidemo:id/comment_send").click();
+        driver.findElementById("neet.com.youjidemo:id/content").sendKeys("bug");
+        driver.findElementById("neet.com.youjidemo:id/send").click();
     }*/
 }
