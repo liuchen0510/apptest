34830d0ec367050cca48fc484d53641a66482e24
刘宸 <825147384@qq.com>
2019-06-21 15:47:01 +0800

    Update share.java

--- liuchen/youji/src/main/java/com/example/youjitest/testcases/share.java
+++ liuchen/youji/src/main/java/com/example/youjitest/testcases/share.java
@@ -1,7 +1,7 @@
 package com.testcases;
 
 import com.base.BaseTest;
-import com.pages.ShareActivity;
+import com.pages.Share;
 
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
