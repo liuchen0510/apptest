08c736cd6aa0d8c3ddd872ae73620aebf4497a72
刘宸 <825147384@qq.com>
2019-06-21 15:25:29 +0800

    Update and rename MessageList.java to List.java

--- liuchen/youji/src/main/java/com/example/youjitest/testcases/List.java
+++ liuchen/youji/src/main/java/com/example/youjitest/testcases/List.java
@@ -6,20 +6,20 @@ import com.pages.MyListView;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class MessageList extends BaseTest {
+public class List extends BaseTest {
     @Test(dataProvider = "")
-    public void loginSuccessful(){
-        MyListView messagePage =new MyListView(getDriver());
+    public void list(){
+        ListPage list =new ListPage(getDriver());
         System.out.print(getDriver().currentActivity());
         Assert.assertEquals(".MainActivity",getDriver().currentActivity());
     }
        /* @Test
-        public void checkma() throws InterruptedException {
+        public void list() throws InterruptedException {
             Thread.sleep(3000);
             driver.findElementByName("消息").click();
             Thread.sleep(3000);
             driver.findElementByName("消息").click();
             driver.findElementByName("@的消息").click();
-            driver.findElementById("neet.com.youjidemo:id/ib_collection").click();
+            driver.findElementById("neet.com.youjidemo:id/collection").click();
 }*/
 }
