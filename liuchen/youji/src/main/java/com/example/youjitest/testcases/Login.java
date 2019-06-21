2ba4e6d8ce95041a18a56d985cf5ef9e99319350
刘宸 <825147384@qq.com>
2019-06-21 15:23:15 +0800

    Update Login.java

--- liuchen/youji/src/main/java/com/example/youjitest/testcases/Login.java
+++ liuchen/youji/src/main/java/com/example/youjitest/testcases/Login.java
@@ -10,19 +10,19 @@ public class Login extends BaseTest {
     @Test(dataProvider = "")
     public void loginSuccessful(){
         LoginPage loginPage =new LoginPage(getDriver());
-        loginPage.login("120331","120331");
+        loginPage.login("15226583265","123456");
         System.out.print(getDriver().currentActivity());
         Assert.assertEquals(".MainActivity",getDriver().currentActivity());
     }
 }
     /*public void loginsuccess() throws InterruptedException {
-      Thread.sleep(5000);
+      Thread.sleep(3000);
       driver.findElementByXPath("//*[@text='我']").click();
-      Thread.sleep(5000);
-      driver.findElementById("neet.com.youjidemo:id/iv_head_sculpture").click();
-      Thread.sleep(5000);
-      driver.findElementById("neet.com.youjidemo:id/et_userPhone").sendKeys("120331");
-      driver.findElement(By.className("android.widget.RelativeLayout")).sendKeys("120331");
-      driver.findElementById("neet.com.youjidemo:id/btn_login").click();
-        Thread.sleep(5000);
+      Thread.sleep(3000);
+      driver.findElementById("neet.com.youjidemo:id/sculpture").click();
+      Thread.sleep(3000);
+      driver.findElementById("neet.com.youjidemo:id/userPhone").sendKeys("15226583265");
+      driver.findElement(By.className("android.widget.RelativeLayout")).sendKeys("123456");
+      driver.findElementById("neet.com.youjidemo:id/login").click();
+      Thread.sleep(3000);
 }*/
